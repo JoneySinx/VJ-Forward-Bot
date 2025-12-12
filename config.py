426551d26f1c3c83f0c492f9057f37e5c68b20c0
𝕊ℙ𝕐𝔻𝔼ℝ <162447832+JoneySinx@users.noreply.github.com>
2025-12-12 21:06:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,29 +1,28 @@
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-from os import environ 
+import os
+from os import environ
 
 class Config:
-    API_ID = int(environ.get("API_ID", ""))
+    # Telegram API Credentials
+    # Get these from https://my.telegram.org
+    API_ID = int(environ.get("API_ID", "0")) 
     API_HASH = environ.get("API_HASH", "")
     BOT_TOKEN = environ.get("BOT_TOKEN", "") 
+    
+    # Session Name (Useful if using string sessions)
     BOT_SESSION = environ.get("BOT_SESSION", "vjbot") 
+    
+    # Database Configuration (MongoDB)
     DATABASE_URI = environ.get("DATABASE_URI", "")
     DATABASE_NAME = environ.get("DATABASE_NAME", "vj-forward-bot")
-    BOT_OWNER = int(environ.get("BOT_OWNER", ""))
+    
+    # Owner ID (Admin)
+    # Default to 0 to prevent crash if not set
+    BOT_OWNER = int(environ.get("BOT_OWNER", "0"))
 
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
-
-class temp(object): 
-    lock = {}
-    CANCEL = {}
-    forwardings = 0
-    BANNED_USERS = []
-    IS_FRWD_CHAT = []
-
-# Don't Remove Credit Tg - @VJ_Botz
-# Subscribe YouTube Channel For Amazing Bot https://youtube.com/@Tech_VJ
-# Ask Doubt on telegram @KingVJ01
+class Temp(object): 
+    # Runtime Variables (बोट चलते समय ये डेटा hold करेंगे)
+    LOCK = {}           # Lock for chat processes
+    CANCEL = {}         # To handle cancellation requests
+    FORWARDINGS = 0     # Counter for active forwards
+    BANNED_USERS = []   # List of banned users
+    IS_FRWD_CHAT = []   # List of chats currently forwarding
